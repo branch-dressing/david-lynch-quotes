1be09a4bcb1d98b3602eb80d8c831b2512f3e630
joelpdurham <joeldur@gmail.com>
2020-01-23 16:22:59 -0800

    able to get all texts

--- __tests__/text.test.js
+++ __tests__/text.test.js
@@ -4,6 +4,7 @@ const request = require('supertest');
 const app = require('../lib/app');
 const connect = require('../lib/utils/connect');
 const mongoose = require('mongoose');
+const Text = require('../lib/models/Text');
 
 describe('app routes', () => {
   beforeAll(() => {
@@ -14,15 +15,20 @@ describe('app routes', () => {
     return mongoose.connection.dropDatabase();
   });
 
+  beforeEach(async() => {
+    await Text.create({ text: 'This is text' });
+  });
+
   afterAll(() => {
     return mongoose.connection.close();
   });
 
   describe('Text Tests', () =>{
+
     it('can create a new text', () => {
       return request(app)
-        .post('/api/v1/text')
-        .send('This is text')
+        .post('/api/v1/texts')
+        .send({ text: 'This is text' })
         .then(res => {
           expect(res.body).toEqual({
             _id: expect.any(String),
@@ -31,5 +37,19 @@ describe('app routes', () => {
           });
         });
     });
+
+    it('can get all texts', () => {
+      return request(app)
+        .get('/api/v1/texts')
+        .then(res => {
+          expect(res.body).toEqual([{
+            _id: expect.any(String),
+            text: 'This is text',
+            __v: 0
+          }]);
+        });
+    });
+
+
   });
 });
