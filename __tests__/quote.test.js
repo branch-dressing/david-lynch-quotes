95af644237b9f3b436055f432c3b04e5e241d6d3
joelpdurham <joeldur@gmail.com>
2020-01-23 19:22:15 -0800

    can create a quote

--- __tests__/quote.test.js
+++ __tests__/quote.test.js
@@ -4,7 +4,7 @@ const request = require('supertest');
 const app = require('../lib/app');
 const connect = require('../lib/utils/connect');
 const mongoose = require('mongoose');
-const Text = require('../lib/models/Text');
+const Quote = require('../lib/models/Quote');
 
 describe('app routes', () => {
   beforeAll(() => {
@@ -15,40 +15,40 @@ describe('app routes', () => {
     return mongoose.connection.dropDatabase();
   });
 
-  beforeEach(async() => {
-    await Text.create({ text: 'This is text' });
-  });
+  // beforeEach(async() => {
+  //   await Text.create({ text: 'This is text' });
+  // });
 
   afterAll(() => {
     return mongoose.connection.close();
   });
 
-  describe('Text Tests', () =>{
+  describe('Quote Tests', () =>{
 
-    it('can create a new text', () => {
+    it('can create a new quote', () => {
       return request(app)
-        .post('/api/v1/texts')
-        .send({ text: 'This is text' })
+        .post('/api/v1/quotes')
+        .send({ quote: 'This is a quote' })
         .then(res => {
           expect(res.body).toEqual({
             _id: expect.any(String),
-            text: 'This is text',
+            quote: 'This is a quote',
             __v: 0
           });
         });
     });
 
-    it('can get all texts', () => {
-      return request(app)
-        .get('/api/v1/texts')
-        .then(res => {
-          expect(res.body).toEqual([{
-            _id: expect.any(String),
-            text: 'This is text',
-            __v: 0
-          }]);
-        });
-    });
+    // it('can get all texts', () => {
+    //   return request(app)
+    //     .get('/api/v1/texts')
+    //     .then(res => {
+    //       expect(res.body).toEqual([{
+    //         _id: expect.any(String),
+    //         text: 'This is text',
+    //         __v: 0
+    //       }]);
+    //     });
+    // });
 
 
   });
