10530c7fffbcd540d5383c144ea98db1cf19bd03
joelpdurham <joeldur@gmail.com>
2020-01-23 16:12:16 -0800

    wrote failing test for text route

--- __tests__/text.test.js
+++ __tests__/text.test.js
@@ -17,4 +17,19 @@ describe('app routes', () => {
   afterAll(() => {
     return mongoose.connection.close();
   });
+
+  describe('Text Tests', () =>{
+    it('can create a new text', () => {
+      return request(app)
+        .post('/api/v1/text')
+        .send('This is text')
+        .then(res => {
+          expect(res.body).toEqual({
+            _id: expect.any(String),
+            text: 'This is text',
+            __v: 0
+          });
+        });
+    });
+  });
 });
